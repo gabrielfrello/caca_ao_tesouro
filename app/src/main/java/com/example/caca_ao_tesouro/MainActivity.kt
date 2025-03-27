5c767212c4868f89ebfbf477fae1f2d62597c326
Gabriel Frello <gabrieltassifrello@hotmail.com>
2025-03-27 00:36:46 -0300

    Aula 05 Mobile

--- app/src/main/java/com/example/caca_ao_tesouro/MainActivity.kt
+++ app/src/main/java/com/example/caca_ao_tesouro/MainActivity.kt
@@ -5,13 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 
 import androidx.navigation.compose.composable
@@ -22,34 +28,52 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val navigationController = rememberNavController()
+            val navController = rememberNavController()
 
             NavHost(
-                navController = navigationController,
-                startDestination = "/home"
+                navController = navController,
+                startDestination = "home"
             ) {
                // Tela01
-                composable (route = "/home") {
-                    Tela(
-                        nomeTela = "Vamos participar de uma caça ao tesouro?",
-                        clickB1 = { navigationController.navigate(route = "/idTela02") },
-                        clickB2 = { navigationController.navigate(route = "/idTela03") }
+                composable ("home") {
+                    TelaHome(
+                        nomeTela = "Home",
+                        clickB1 = { navController.navigate("Tela01") }
                     )
                 }
                 //Tela02
-                composable (route = "/idTela02") {
-                    Tela(
-                        nomeTela = "Quanto é 1+1?",
-                        clickB1 = { navigationController.navigate(route = "/home") },
-                        clickB2 = { navigationController.navigate(route = "/idTela03") }
+                composable ("Tela01") {
+                    TelaPergunta(
+                        nomeTela = "Pergunta01",
+                        pergunta = "Quanto é 1+1?",
+                        resposta = "2",
+                        proximo = { navController.navigate("Tela02")},
+                        anterior = { navController.navigate("Home") }
                     )
                 }
                 //Tela03
-                composable (route = "/idTela03") {
-                    Tela(
-                        nomeTela = "Tela03",
-                        clickB1 = { navigationController.navigate(route = "/idTela02") },
-                        clickB2 = { navigationController.navigate(route = "/idTela03") }
+                composable ("Tela02") {
+                    TelaPergunta(
+                        nomeTela = "Pergunta02",
+                        pergunta = "Quanto é 2+2?",
+                        resposta = "4",
+                        proximo = { navController.navigate("Tela03")},
+                        anterior = { navController.navigate("Tela01") }
+                    )
+                }
+                //Tela03
+                composable ("Tela03") {
+                    TelaPergunta(
+                        nomeTela = "Pergunta03",
+                        pergunta = "Quanto é 3+3?",
+                        resposta = "6",
+                        proximo = { navController.navigate("TelaPremio")},
+                        anterior = { navController.navigate("Tela02") }
+                    )
+                }
+                composable ("TelaPremio") {
+                    TelaFinal(
+                        clickB1 = { navController.navigate("Tela01") }
                     )
                 }
             }
@@ -58,19 +82,76 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Tela(
+fun TelaHome(
     nomeTela: String,
     clickB1: () -> Unit,
-    clickB2: () -> Unit
 ) {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(nomeTela)
+        Text(text = "Que tal uma caça ao tesouro?")
         Button(onClick = clickB1) { Text(text = "Iniciar caça ao tesouro") }
-        Button(onClick = clickB2) { Text(text = "B2") }
+    }
+}
+
+@Composable
+fun TelaFinal(
+    clickB1: () -> Unit,
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Parabéns, caça ao tesouro finalizada!")
+        Button(onClick = clickB1) { Text(text = "Voltar a Home") }
+    }
+}
+
+@Composable
+fun TelaPergunta(
+    nomeTela: String,
+    pergunta: String,
+    resposta: String,
+    proximo: () -> Unit,
+    anterior: () -> Unit
+) {
+    val (textoDigitado, setTextoDigitado) = remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = pergunta)
+
+        TextField(
+            value = textoDigitado,
+            onValueChange = {
+                setTextoDigitado(it)
+            },
+            label = { Text("Digite a resposta") },
+            modifier = Modifier.padding(top = 24.dp)
+        )
+
+        Row(
+            modifier = Modifier.padding(top = 25.dp),
+            horizontalArrangement = Arrangement.spacedBy(20.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Button(onClick = {
+                if (textoDigitado.lowercase() == resposta.lowercase()) {
+                    proximo()
+                }
+            }) {
+                Text("Próximo")
+            }
+            Button(onClick = anterior) {
+                Text("Voltar")
+            }
+        }
     }
 }
 
@@ -78,5 +159,18 @@ fun Tela(
 @Preview(showBackground = true)
 @Composable
 fun Teste() {
-    Tela(nomeTela = "Tela teste", {}, {})
+    TelaPergunta(
+        nomeTela = "Pergunta01",
+        pergunta = "Quanto é 1+1?",
+        resposta = "2",
+        proximo = {},
+        anterior = {}
+    )
+    TelaHome(
+        nomeTela = "Home",
+        clickB1 =  {}
+    )
+    TelaFinal(
+        clickB1 = {}
+    )
 }
